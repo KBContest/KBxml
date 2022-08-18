992e9ee71be84fa1c2b658dc54ae656a1ce53419
YURIM LEE <yur510@naver.com>
2022-08-18 22:58:20 +0900

    회원가입 xml 완성

---
+++ app/src/main/java/com/example/kb/SignUpActivity4.kt
@@ -0,0 +1,12 @@
+/*회원가입_4*/
+package com.example.kb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up4)
+    }
+}
---
+++ app/src/main/java/com/example/kb/SignUpActivity6.kt
@@ -0,0 +1,12 @@
+/*회원가입_6*/
+package com.example.kb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up6)
+    }
+}
