848695da1e5639b45c8645c5bbca5dd0f172087f
YURIM LEE <yur510@naver.com>
2022-08-23 23:43:37 +0900

    질의응답 xml 완료

---
+++ app/src/main/java/com/example/kb/QaActivity2.kt
@@ -0,0 +1,12 @@
+/*질의응답_2*/
+package com.example.kb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QaActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qa2)
+    }
+}
