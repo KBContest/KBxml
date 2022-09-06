795de4507550d349dc98ce199624a3d046add5e5
YURIM LEE <yur510@naver.com>
2022-09-06 23:33:00 +0900

    내용 추가 및 불필요한 페이지 삭제

--- app/src/main/java/com/example/kb/MypageFragment.kt
+++ app/src/main/java/com/example/kb/MypageFragment.kt
@@ -1,18 +1,13 @@
 /*마이페이지_1*/
 package com.example.kb
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 class MypageFragment : Fragment() {
-
-    lateinit var wrote_btn:Button
-    lateinit var fund_btn:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +20,6 @@ class MypageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_mypage, container, false)
-
-
-        /*xml 확인위해 임시 연결(삭제해도 됨)*/
-        wrote_btn = view.findViewById(R.id.wrote_btn)
-        fund_btn = view.findViewById(R.id.fund_btn)
-
-        wrote_btn.setOnClickListener{
-            val intent = Intent(context, MypageWroteActivity::class.java)
-            startActivity(intent)
-        }
-        fund_btn.setOnClickListener{
-            val intent = Intent(context, MypageFundActivity::class.java)
-            startActivity(intent)
-        }
-        /*xml 확인위해 임시 연결(삭제해도 됨)*/
 
         return view
     }
